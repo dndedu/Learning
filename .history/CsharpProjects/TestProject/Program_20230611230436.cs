c5fa35f122c3fc8bf2222b6dd96caae66ecd1cfc
dadom002 <daniel.domanski002@stud.fh-dortmund.de>
2023-06-12 01:42:52 +0200

    MS C# learnpath continued

---
+++ .history/CsharpProjects/TestProject/Program_20230611230436.cs
@@ -0,0 +1,9 @@
+﻿int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
+foreach (int items in inventory)
+{
+    sum += items;
+}
+
+Console.WriteLine($"We have {sum} items in inventory.");
---
+++ .history/CsharpProjects/TestProject/Program_20230611231909.cs
@@ -0,0 +1,1 @@
+﻿string[] potentiallyFraudulentOrders = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
