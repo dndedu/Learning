00f2d083bccc58885bce51a884d348a352f8edee
dndedu <daniel.domanski@udo.edu>
2023-06-12 16:40:17 +0200

    just commit&push

---
+++ .history/CsharpProjects/TestProject/Program_20230612152413.cs
@@ -0,0 +1,12 @@
+﻿/* string[] potentiallyFraudulentOrders = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string pFO in potentiallyFraudulentOrders)
+{
+    if (pFO.StartsWith('B'))
+    {
+        Console.WriteLine(pFO);
+    }
+} */
+
+int[] a = { 1, 2, 3 };
+a[3] = 4;
