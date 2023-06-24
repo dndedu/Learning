2f792c8910dc80b44be25b287e0b9d0b7ff87779
dadom002 <daniel.domanski002@stud.fh-dortmund.de>
2023-06-24 15:49:25 +0200

    MS C# GetStarted Challenge3 done

---
+++ .history/CsharpProjects/tmp/Program_20230624133852.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string a = Console.ReadLine();
+console.WriteLine(a);
+
---
+++ .history/CsharpProjects/tmp/Program_20230624133908.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string a = Console.ReadLine();
+Console.WriteLine(a);
