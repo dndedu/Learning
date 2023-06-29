93954ed6d28344558c7fa426224310a6be5d2f32
dndedu <daniel.domanski@udo.edu>
2023-06-29 22:03:40 +0200

    MS C# ToCharArray, new string, Join, Split https://learn.microsoft.com/en-us/training/modules/csharp-arrays-operations/5-challenge-1

--- .history/CsharpProjects/TestProject/Program_20230629212240.cs
+++ .history/CsharpProjects/TestProject/Program_20230629212240.cs
@@ -49,12 +49,6 @@ foreach (var pallet in pallets)
 string value = "abc123";
 char[] valueArray = value.ToCharArray();
 Array.Reverse(valueArray);
-// string result = new string(valueArray);
-string result = String.Join(",", valueArray);
+Console.WriteLine(valueArray);
+string result = new string(valueArray);
 Console.WriteLine(result);
-
-string[] items = result.Split(',');
-foreach (string item in items)
-{
-    Console.WriteLine(item);
-}
