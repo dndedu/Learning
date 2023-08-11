9b8649f1f20a744beb1df4e7131e38d1602f2171
dndedu <daniel.domanski@udo.edu>
2023-08-11 20:48:59 +0200

    1 added example for C 2 set up environment variables gcc/make

---
+++ Projects_0_C/PK2/2 Rekursive Funktion, Präprozessor/summe.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define DEBUG 1
+#define LOG(text) printf("Datei %s, Zeile %d: %s\n", __FILE__, __LINE__, text)
+
+#define MIN(a, b) (a < b) ? a : b
+
+int summe(int n)
+{
+  if (n == 0)
+    return 0;
+  return n + summe(n - 1);
+}
+
+int main()
+{
+
+#if DEBUG
+  LOG("Vor Summe");
+#endif
+  printf("Summe = %d\n", summe(100));
+  printf("%d\n", MIN(100, 50));
+}
