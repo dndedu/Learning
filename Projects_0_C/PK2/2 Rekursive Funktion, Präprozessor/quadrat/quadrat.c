07b809473b3a34b9a852de6becc9fef79a51b30c
dadom002 <daniel.domanski002@stud.fh-dortmund.de>
2023-08-15 08:04:02 +0200

    C, function pointer

---
+++ Projects_0_C/PK2/2 Rekursive Funktion, Präprozessor/quadrat/quadrat.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+long summe(int n){
+  if (n == 0) return 0;
+  return n + summe(n-1);
+}
+
+/*
+schlechte Loesung
+void quadrat(int x){
+  printf("%d\n", x*x);
+}
+*/
+
+// gute Loesung
+int quadrat(int x){
+  return x*x;
+}
+
+void print_quadrat(int x){
+  printf("%d\n", quadrat(x));
+}
+
+int main(){
+  
+  print_quadrat(2);
+  int a = 2 * quadrat(2);
+ 
+  printf("Summe(%d) = %ld\n", a, summe(a));
+  return 0;
+}
--- Projects_0_C/PK2/3 Felder, Zeichenketten, Strukturen, Varianten, Speicherklassen/struct/punkt.c
+++ Projects_0_C/PK2/3 Felder, Zeichenketten, Strukturen, Varianten, Speicherklassen/struct/punkt.c
@@ -2,40 +2,46 @@
 
 typedef struct punkt punkt_s;
 
-struct punkt{
+struct punkt
+{
     int x;
     int y;
 };
 
-struct rechteck{
+struct rechteck
+{
     punkt_s p1;
     punkt_s p2;
 };
 
-struct rechteck create_rechteck(int x1, int y1, int x2, int y2){
-   struct rechteck r;
-   r.p1.x = x1;
-   r.p1.y = y1;
-   r.p2.x = x2;
-   r.p2.y = y2;
-   return r;
+struct rechteck create_rechteck(int x1, int y1, int x2, int y2)
+{
+    struct rechteck r;
+    r.p1.x = x1;
+    r.p1.y = y1;
+    r.p2.x = x2;
+    r.p2.y = y2;
+    return r;
 }
 
-int flaeche(struct rechteck r){
+int flaeche(struct rechteck r)
+{
     return (r.p2.x - r.p1.x) * (r.p1.y - r.p2.y);
 }
 
-void print_punkt(struct punkt p){
+void print_punkt(struct punkt p)
+{
     printf("(%d, %d)\n", p.x, p.y);
 }
 
-int main(){
+int main()
+{
     struct punkt p;
     p.x = 100;
     p.y = 200;
     print_punkt(p);
 
-    struct rechteck r = create_rechteck(100,110, 110, 100);
+    struct rechteck r = create_rechteck(100, 110, 110, 100);
     printf("Fläche = %d\n", flaeche(r));
 
     return 0;
